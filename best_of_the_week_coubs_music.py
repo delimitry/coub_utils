25056f7ea682ddeafc04ec638daad487fdd50994
Dmitry Alimov <dvalimov@gmail.com>
2021-10-05 11:20:07 +0300

    Update best_of_the_week_coubs_music.py

--- best_of_the_week_coubs_music.py
+++ best_of_the_week_coubs_music.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import csv
 import json
 import multiprocessing
-import requests
 import sys
+
+import requests
 
 PY3 = sys.version_info[0] == 3
 
@@ -17,10 +18,10 @@ WEEKLY_DIGESTS_URL = COUB_URL + '/api/v2/weekly_digests'
 def get_audio_track_titles(media_blocks):
     """Get audio track titles"""
     if not media_blocks:
-        return ('', '')
+        return '', ''
     audio_track_title = media_blocks.get('audio_track', {}).get('title', '')
     external_video_title = media_blocks.get('external_video', {}).get('title', '')
-    return (audio_track_title, external_video_title)
+    return audio_track_title, external_video_title
 
 
 def get_weekly_digests():
@@ -39,8 +40,7 @@ def get_weekly_digest_coubs_on_page(index, page_id):
 def get_all_weekly_digest_coubs(index):
     """Get all weekly digest coubs (from all pages)"""
     req = requests.get('{url}/{index}/coubs'.format(url=WEEKLY_DIGESTS_URL, index=index))
-    page_data = json.loads(req.text)
-    per_page = page_data.get('per_page')
+    page_data = req.json()
     page = page_data.get('page')
     total_pages = page_data.get('total_pages')
     all_coubs = [page_data]
@@ -55,7 +55,7 @@ def get_all_weekly_digest_coubs(index):
 
 def main():
     print('-' * 80)
-    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() * 4) 
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() * 4)
     weekly_digests = get_weekly_digests()
     weekly_digest_ids = [weekly_digest.get('id') for weekly_digest in weekly_digests]
     all_weekly_coubs_pages_res = pool.map(get_all_weekly_digest_coubs, weekly_digest_ids)
